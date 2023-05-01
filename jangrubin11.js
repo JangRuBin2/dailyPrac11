4e60cda07dc0119ab458abb5ef3b20fd9e6912a0
장루빈집 <wkdfnqls2465@gmail.com>
2023-05-01 23:11:48 +0900

    item이 배열임을 확인해야 하지 않나?

--- jangrubin11.js
+++ jangrubin11.js
@@ -1,21 +1,27 @@
 example = [[1,2,3], [4,[5,6]],7, [8,9]];
-// console.log(typeof example);
+console.log(typeof example[0]);
 function flatten(data) {
   let output = [];
-  console.log(typeof(example[3]))
+  // console.log(typeof(example[0]))
+// console.log(example.join())
   for (let item in data) {
     // object 형태면 output에 분해 된 아이템을 넣는 건가?
     // ? 다차원 배열을 일차원 배열로 만드는게 flatten?
     // ! 배열 길이만큼 실행
     // item이 배열 형태면 벗겨내서 example을 일차원 배열로 만드는게 목표
-    if (typeof(item) == "object") {
-      // ?? 배열을 
+    if (Array.isArray(item)) {
       // let test = data.falt();
+      // flattenObject = item.join();
+      // output.push(flattenObject) ;
+      // output += flattenObject;
       // let test = Object.values(data)
       // let test = data.slice(0,4)
-      
-      console.log(test)
+      console.log("test")
+
+    } else {
+      // 배열이나 object가 아니면 바로 push함
+      output.push(item);
     }
-  }     
+  } return output;
 };
 console.log(flatten(example));
